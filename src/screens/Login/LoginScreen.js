f0a51b7f19f887bff476fcc9dd4e36670eb3729f
stanmelchizedek <stanexseven@gmail.com>
2024-01-26 10:04:07 +0300

    new change

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -37,7 +37,7 @@ const LoginScreen = ({ navigation }) => {
       console.log(email, password);
       Alert.alert("Login Successsfully")
     } catch (error) {
-      console.error("Login failed: ", error.message);
+      console.error("Login failed with the following error: ", error.message);
     }
   };
 
