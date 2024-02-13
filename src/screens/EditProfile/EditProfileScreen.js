56ec9db548b0afbf25ca68542c1a0965ba62aa35
stanmelchizedek <stanexseven@gmail.com>
2024-02-13 14:06:46 +0300

    Changed aspect ratio for images

--- src/screens/EditProfile/EditProfileScreen.js
+++ src/screens/EditProfile/EditProfileScreen.js
@@ -84,7 +84,7 @@ const EditProfileScreen = ({ navigation }) => {
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
@@ -100,7 +100,7 @@ const EditProfileScreen = ({ navigation }) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
